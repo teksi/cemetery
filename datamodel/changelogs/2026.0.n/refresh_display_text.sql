f9861e2ab2a13420696c3b6f669110e4b510312c
bes <schuepbachjamin@gmail.com>
2026-03-04 10:58:28 +0100

    add update of display-text

---
+++ datamodel/changelogs/2026.0.n/refresh_display_text.sql
@@ -0,0 +1,11 @@
+-- this is used to update display-text attributes
+UPDATE tce_od.cemetery SET remark = remark;
+UPDATE tce_od.contact SET remark = remark;
+UPDATE tce_od.contract SET remark = remark;
+UPDATE tce_od.deceased SET remark = remark;
+UPDATE tce_od.file SET remark = remark;
+UPDATE tce_od.furniture SET remark = remark;
+UPDATE tce_od.organisation SET remark = remark;
+UPDATE tce_od.sector SET remark = remark;
+UPDATE tce_od.unit SET remark = remark;
+UPDATE tce_od.vegetation SET remark = remark;
