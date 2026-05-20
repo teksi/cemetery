280acf1e5eb1c55689f7fd9ea1a8faacdfc490c9
Arnaud Poncet-Montanges <ponceta@users.noreply.github.com>
2026-05-20 08:43:32 +0200

    Add relation and contact link

--- datamodel/changelogs/2026.0.2/02_update_deceased.sql
+++ datamodel/changelogs/2026.0.2/02_update_deceased.sql
@@ -2,3 +2,13 @@
 ALTER TABLE tce_od.deceased ADD COLUMN IF NOT EXISTS ceremony_place TEXT;
 ALTER TABLE tce_od.deceased ADD COLUMN IF NOT EXISTS ceremony_date DATE;
 ALTER TABLE tce_od.deceased ADD COLUMN IF NOT EXISTS fk_contact_person uuid;
+
+ALTER TABLE tce_od.deceased
+  ADD CONSTRAINT rel_deceased_contact_person FOREIGN KEY (fk_contact_person)
+  REFERENCES tce_od.contact (id) MATCH SIMPLE
+  ON UPDATE RESTRICT ON DELETE RESTRICT
+  DEFERRABLE INITIALLY DEFERRED;
+
+COMMENT ON CONSTRAINT rel_deceased_contact_person ON tce_od.deceased IS 'Foreign key constraint linking deceased.fk_contact_person to contact.id, ensuring referential integrity between deceased records and their associated contact.';
+
+ALTER TABLE tce_od.deceased ADD COLUMN IF NOT EXISTS contact_person_link TEXT;
